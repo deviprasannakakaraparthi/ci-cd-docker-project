4bf8d87d312ffba5315c1e9a53b79b0fb37f52a9
prasanna-kakaraparthi <160647896+deviprasannakakaraparthi@users.noreply.github.com>
2025-05-11 21:23:45 +0530

    Update app.py

--- app2/app.py
+++ app2/app.py
@@ -1,9 +1,6 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return "Hello from App2!"
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+@app.route("/")
+def home():
+    return "Hello from App 2 - UPDATED!"
